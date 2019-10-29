1f5c81817d91c7085e2aa6d940342baefb447833
Nickolas Da Silva <nickolas.silva@researchgate.net>
2019-10-29 20:46:54 +0100

    Initial commit

---
+++ source/_layouts/master.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+
+  <title>The PHP Website</title>
+</head>
+<body class="yue">
+@yield('body')
+</body>
+</html>
