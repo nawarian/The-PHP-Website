24807c4a20e2ca0334813674eb0ca8c44fe02f82
Nickolas Da Silva <nickolas.silva@researchgate.net>
2019-10-31 17:53:43 +0100

    Add sitemap generator package and configure after build step

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 use TightenCo\Jigsaw\Jigsaw;
+use PODEntender\SitemapGenerator\Adapter\Jigsaw\JigsawAdapter;
 
 /** @var $container \Illuminate\Container\Container */
 /** @var $events \TightenCo\Jigsaw\Events\EventBus */
@@ -15,6 +16,23 @@ use TightenCo\Jigsaw\Jigsaw;
  *     // Your code here
  * });
  */
+
 $events->afterCollections(function (Jigsaw $app) {
     $app->setConfig('latestIssues', $app->getCollection('posts_en')->take(5));
 });
+
+$events->afterBuild(function (Jigsaw $jigsaw) {
+    $outputPath = $jigsaw->getDestinationPath();
+
+    // Fetches the adapter from Dependency Injection
+    $sitemapGenerator = $jigsaw->app->get(JigsawAdapter::class);
+
+    // English posts
+    $englishPosts = $jigsaw->getCollection('posts_en');
+    file_put_contents($outputPath . '/sitemap-en.xml', $sitemapGenerator->fromCollection($englishPosts)->saveXML());
+
+    // Portuguese posts
+    $portuguesePosts = $jigsaw->getCollection('posts_pt_br');
+    file_put_contents($outputPath . '/sitemap-pt-br.xml', $sitemapGenerator->fromCollection($portuguesePosts)->saveXML());
+});
+
