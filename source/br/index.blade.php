c55ca717550539969d9844744231d2c27de911ef
nawarian <nickolas@phpsp.org.br>
2019-12-20 14:48:14 +0100

    style(cards): add card style for home pages

--- source/br/index.blade.php
+++ source/br/index.blade.php
@@ -6,19 +6,25 @@
 
 @section('body')
 <h1>Últimas postagens</h1>
-<ul>
+<ul class="card-list">
   @foreach($page->get('latestIssuesBr') as $post)
-    <li>
-      <a href="{{ $post->getUrl() }}">[{{ date('Y-m-d', $post->createdAt) }}] - {{ $post->title }}</a>
+    <li class="card">
+      <a href="{{ $post->getUrl() }}">
+        <h3>{{ $post->title }}</h3>
+        <p>{{ $post->meta['description'] }}</p>
+        <small>{{ date('d/m/Y', $post->createdAt) }}</small>
+      </a>
     </li>
   @endforeach
 </ul>
 
-<h1>Vagas de Emprego de programação em PHP</h1>
-<ul>
+<hr />
+<h2>Vagas de Emprego de programação em PHP</h2>
+<ul class="card-list">
   @foreach($page->get('latestJobsBr') as $post)
-    <li>
-      <a href="{{ $post->getUrl() }}">[{{ date('Y-m-d', $post->createdAt) }}] - {{ $post->title }}</a>
+    <li class="card">
+      <a href="{{ $post->getUrl() }}"><h3>{{ $post->title }}</h3></a>
+      <small>Publicado em {{ date('d/m/Y', $post->createdAt) }}</small>
     </li>
   @endforeach
 </ul>
