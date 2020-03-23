2c94840061fd4b4fdd84fc793ef48dd6aba924b0
Nawarian <nickolas@phpsp.org.br>
2020-03-24 00:21:22 +0100

    Add internal links quality gate (not active yet)

--- app/src/PostQualityVerifier.php
+++ app/src/PostQualityVerifier.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace Nawarian\ThePHPWebsite;
 
 use Exception;
+use Illuminate\Support\Str;
 use Symfony\Component\DomCrawler\Crawler;
 
 class PostQualityVerifier
 {
+    private const MIN_INTERNAL_LINKS = 2;
+
     private $domCrawler;
 
     public function __construct(Crawler $crawler)
@@ -21,13 +24,39 @@ class PostQualityVerifier
         $this->domCrawler->clear();
         $this->domCrawler->addContent($html);
 
-        $this->verifyMultipleH1Tags($html);
+        $this->verifyMultipleH1Tags();
+        // $this->verifyInternalLinks();
     }
 
-    private function verifyMultipleH1Tags(string $html): void
+    private function verifyMultipleH1Tags(): void
     {
-        if ($this->domCrawler->filter('h1')->count() > 1) {
+        if ($this->domCrawler->filter('article.container h1')->count() > 1) {
             throw new Exception('There should be only one h1 tag in the whole post.');
+        }
+    }
+
+    private function verifyInternalLinks(): void
+    {
+        $pattern = 'Article contains %s internal links. At least %d are required.';
+        if ($this->domCrawler->filter('article.container a')->count() === 0) {
+            throw new Exception(
+                sprintf($pattern, 'no', self::MIN_INTERNAL_LINKS)
+            );
+        }
+
+        $found = 0;
+        foreach ($this->domCrawler->filter(' article.container a') as $link) {
+            $href = $link->getAttribute('href') ?? '';
+
+            if (Str::startsWith($href, ['/', 'https://thephp.website'])) {
+                $found++;
+            }
+        }
+
+        if ($found < self::MIN_INTERNAL_LINKS) {
+            throw new Exception(
+                sprintf($pattern, $found, self::MIN_INTERNAL_LINKS)
+            );
         }
     }
 }
--- app/tests/Nawarian/ThePHPWebsite/PostQualityVerifierTest.php
+++ app/tests/Nawarian/ThePHPWebsite/PostQualityVerifierTest.php
@@ -22,15 +22,71 @@ class PostQualityVerifierTest extends TestCase
     public function testVerifyErrorsIfMoreThanOneH1TagIsFound(): void
     {
         $multipleH1TagsHtml = <<<STR
-<h1>the first title in the page</h1>
-<p>some content here</p>
-<h1>another title, should bring an exception</h1>
-<p>another content here</p>
+<article class="container">
+    <h1>the first title in the page</h1>
+    <p>some content here</p>
+    <h1>another title, should bring an exception</h1>
+    <p>another content here</p>
+</article>
 STR;
 
         self::expectException(Exception::class);
         self::expectExceptionMessage('There should be only one h1 tag in the whole post.');
 
         $this->verifier->verify($multipleH1TagsHtml);
+    }
+
+    public function testVerifyErrorsIfParagraphIsTooShort(): void
+    {
+        self::markTestSkipped('This feature must be better thought through.');
+        $shortParagraphHtml = <<<STR
+<article class="container">
+    <h1>the first title in the page</h1>
+    <p>some content here</p>
+</article>
+STR;
+
+        self::expectException(Exception::class);
+        self::expectExceptionMessage(
+            'Paragraphs should contain at least 65 words. '
+            . "The offending paragraph was 'some content here'."
+        );
+
+        $this->verifier->verify($shortParagraphHtml);
+    }
+
+    public function testVerifyErrorsWhenNoInternalLinksAreFound(): void
+    {
+        self::markTestSkipped('Cant activate this feature yet.');
+        $noInternalLinksHtml = <<<STR
+<article class="container">
+    <h1>the first title in the page</h1>
+</article>
+STR;
+
+        self::expectException(Exception::class);
+        self::expectExceptionMessage(
+            'Article contains no internal links. At least 2 are required.'
+        );
+
+        $this->verifier->verify($noInternalLinksHtml);
+    }
+
+    public function testVerifyErrorsWhenNotEnoughInternalLinksAreFound(): void
+    {
+        self::markTestSkipped('Cant activate this feature yet.');
+        $noInternalLinksHtml = <<<STR
+<article class="container">
+    <h1>the first title in the page</h1>
+    <a href="/en/issue/test">Testing</a>
+</article>
+STR;
+
+        self::expectException(Exception::class);
+        self::expectExceptionMessage(
+            'Article contains 1 internal links. At least 2 are required.'
+        );
+
+        $this->verifier->verify($noInternalLinksHtml);
     }
 }
