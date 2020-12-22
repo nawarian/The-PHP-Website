14a6bb2e00a42f5f0f3eacf91a4f652dbd671ac4
Níckolas Daniel da Silva <nickolas@phpsp.org.br>
2020-12-22 16:08:08 +0100

    Add canonical link to page templates

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -5,6 +5,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+  @if ($page->canonical)
+    <link rel="canonical" href="{{ $page->canonical }}" />
+  @elseif ($page->link)
+    <link rel="canonical" href="{{ $page->getUrl() }}/" />
+  @else
+    <link rel="canonical" href="{{ $page->getUrl() }}" />
+  @endif
   <link
     rel="alternate"
     type="application/json"
