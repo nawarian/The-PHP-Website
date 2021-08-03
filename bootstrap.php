a0888a04c0bd434acb13e9c50be1e7de31ba0c43
Níckolas Daniel da Silva <nawarian@gmail.com>
2021-08-03 12:33:06 +0200

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -15,6 +15,7 @@ use TightenCo\Jigsaw\PageVariable;
 /** @var $container \Illuminate\Container\Container */
 /** @var $events \TightenCo\Jigsaw\Events\EventBus */
 
+
 /**
  * You can run custom code at different stages of the build process by
  * listening to the 'beforeBuild', 'afterCollections', and 'afterBuild' events.
